31f16b2a189c4a468cf45cc24e30eeb7f29ad8a3
백대홍 <bdh3620@naver.com>
2022-10-21 16:00:29 +0900

    useState part

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+
+  
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','ㅁㄴㅇㅁㄴㅇ']);
+  let [글제목1, 글제목변경1] = useState(['백대홍','할수있겠나']);
+  let [글제목2, 글제목변경2] = useState(['백대홍','나를 소개하지']);
+
+  let post = '진주시 고기 맛집';
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3> { 글제목 } </h3>
+        <p>10월 21일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목1 } </h3>
+        <p>10월 21일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목2 } </h3>
+        <p>10월 21일 발행</p>
+        <hr/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
