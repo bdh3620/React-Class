1510d0a2220ec1031b8856a5ba13d616ab1e08c1
백대홍 <bdh3620@naver.com>
2022-10-21 18:29:09 +0900

    button listener

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/*eslint-disable  */
+
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
@@ -5,10 +7,8 @@ import React, { useState } from 'react';
 function App() {
 
   
-  let [글제목, 글제목변경] = useState(['남자 코트 추천','ㅁㄴㅇㅁㄴㅇ']);
-  let [글제목1, 글제목변경1] = useState(['','할수있겠나']);
-  let [글제목2, 글제목변경2] = useState(['','나를 소개하지']);
-
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','진주시 고기 맛집','파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
   let post = '진주시 고기 맛집';
 
   return (
@@ -17,17 +17,18 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> { 글제목 } </h3>
+        <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span>
+         {따봉} </h3>
         <p>10월 21일 발행</p>
         <hr/>
       </div>
       <div className="list">
-        <h3> { 글제목1 } </h3>
+        <h3> { 글제목[1] } </h3>
         <p>10월 21일 발행</p>
         <hr/>
       </div>
       <div className="list">
-        <h3> { 글제목2 } </h3>
+        <h3> { 글제목[2] } </h3>
         <p>10월 21일 발행</p>
         <hr/>
       </div>
