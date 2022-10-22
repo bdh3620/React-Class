c2a28a1f39e224cd21fe83ad6aa0813424f6e636
백대홍 <bdh3620@naver.com>
2022-10-22 14:55:01 +0900

    Button function

--- src/App.js
+++ src/App.js
@@ -10,12 +10,18 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천','진주시 고기 맛집','파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
   let post = '진주시 고기 맛집';
+  function titlechange(){
+    var newArray =[...글제목];
+    newArray[0] = '여자코트 추천';
+    글제목변경(newArray);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <button onClick={ titlechange } > 버튼 </button>
       <div className="list">
         <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span>
          {따봉} </h3>
